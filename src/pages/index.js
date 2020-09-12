1879dd84bb3a65962c4b6fc66c06acf6b9058177
Denis Toledo <contato@denistoledo.com.br>
2020-09-12 10:27:46 -0300

    install styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
 export default function Home() {
-  return <h1>Soon =)</h1>
+  return (
+    <Container>
+      <h1>Soon =)</h1>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
