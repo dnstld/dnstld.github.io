cc9cbf9f2d96ba13265ef323625e572d60477665
Denis Toledo <contato@denistoledo.com.br>
2020-09-12 10:56:06 -0300

    install tailwind and purgecss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import styled from 'styled-components';
+import tw, { styled } from 'twin.macro'
 
 export default function Home() {
   return (
     <Container>
-      <h1>Soon =)</h1>
+      <Title>Soon =)</Title>
     </Container>
   )
 }
 
 const Container = styled.div`
-  margin: 3rem auto;
+  margin: 5rem auto;
   max-width: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`
+
+const Title = tw.h1`
+  bg-blue-500 hover:bg-blue-800 text-white p-2 rounded
 `
