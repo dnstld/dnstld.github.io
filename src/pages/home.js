d9d8c23723989020a69eafb4c1eff783668a2942
Denis Toledo <contato@denistoledo.com.br>
2020-09-28 09:00:42 -0300

    update intro text

--- src/pages/home.js
+++ src/pages/home.js
@@ -32,8 +32,8 @@ const HomePage = () => (
     <Info>
       <FontAwesomeIcon icon={faQuoteLeft} size="3x" className="text-primary" />
       <p>
-        I am a frontend developer with hands-on experience with HTML5, CSS3, and
-        modern JavaScript, including 4+ years of working with Ember.js.
+        I am a frontend developer well-versed in HTML5, CSS3, and hands-on
+        experience with modern JavaScript, including 4+ years of working with Ember.js.
       </p>
       <p>
         An enthusiast user experience and user interface designer with strong
